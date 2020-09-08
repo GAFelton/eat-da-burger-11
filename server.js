a2769872056cd78c9f27e619ff43a7cda9d36dbb
George F <64555318+GAFelton@users.noreply.github.com>
2020-09-08 12:13:52 -0700

    Removed connection from server.js and moved it to connection.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+
 
 var app = express();
 
@@ -12,24 +12,6 @@ app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "apassword",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-});
-
 
 
 
