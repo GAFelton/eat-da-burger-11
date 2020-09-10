2e0dc430e37086010bdc23a7e12f274e757816c5
George F <64555318+GAFelton@users.noreply.github.com>
2020-09-10 12:18:35 -0700

    Make ORM generic, added helper function for inputting the correct number of '?'

---
+++ config/orm.js
@@ -0,0 +1,61 @@
+const connection = require("./connection");
+
+// Helper function for printing the correct number of question marks.
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+
+const orm = {
+    // GET METHOD
+    selectAll: function (tableName, cb) {
+        var queryString = "SELECT * FROM " + tableName + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) { throw err; }
+            cb(result);
+        });
+    },
+
+    // POST METHOD
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO" + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if(err) {throw err}
+            cb(result);
+        });
+    },
+
+    // PUT METHOD
+    updateOne: function(table, valsToSet, condition, cb) {
+        var queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += printQuestionMarks(valsToSet.length);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, valsToSet, function(err, result) {
+            if (err) {throw err};
+            cb(result);
+        });
+    }
+
+};
+
+module.exports = orm;
