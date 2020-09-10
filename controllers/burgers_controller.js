b96f2640752ae0e2abda7bd73dfe907376d6f8cb
George F <64555318+GAFelton@users.noreply.github.com>
2020-09-10 12:34:50 -0700

    Filled out burger controller with get index, post, and put methods.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+var router = express.Router();
+
+const burger = require("../models/burger");
+
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var objForHandlebars = {
+            burgers: data
+        };
+        console.log(objForHandlebars);
+        res.render("index", objForHandlebars);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.addBurger(
+        ["burger_name, devoured"],
+        [req.body.name, req.body.devoured],
+        function (result) {
+            res.json({ id: result.id });
+        });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("id of burger to be devoured: ", condition);
+
+    burger.update({ devoured: req.body.devoured }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
