b21add72553ae6ddfc48a5339ca760d0f17d2dc3
George F <64555318+GAFelton@users.noreply.github.com>
2020-09-10 16:33:56 -0700

    Added mysql database connection for JawsDB.

--- config/connection.js
+++ config/connection.js
@@ -2,22 +2,28 @@
 
 const util = require('util');
 const mysql = require('mysql');
+var connection;
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'apassword',
     database: 'burgers_db'
-});
-
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-  
-    console.log("connected as id " + connection.threadId);
   });
+}
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
 
 // Setting up connection.query to use promises instead of callbacks.
 // This allows us to use the async/await syntax.
