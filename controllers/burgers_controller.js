83821e0b49714afc470bd7b64d9336fd899015e4
George F <64555318+GAFelton@users.noreply.github.com>
2020-09-10 16:17:29 -0700

    Added function to address problem with JSON object not properly carrying Boolean value, fixed references to devoured boolean.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,8 @@ var router = express.Router();
 
 const burger = require("../models/burger");
 
+const convertStringBoolean = (str) => str == "true";
+
 router.get("/", function (req, res) {
     burger.selectAll(function (data) {
         var objForHandlebars = {
@@ -15,9 +17,10 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
+    const devoured = convertStringBoolean(req.body.devoured);
     burger.addBurger(
         ["burger_name, devoured"],
-        [req.body.name, req.body.devoured],
+        [req.body.name, devoured],
         function (result) {
             res.json({ id: result.id });
         });
@@ -25,10 +28,11 @@ router.post("/api/burgers", function (req, res) {
 
 router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
+    const devoured = convertStringBoolean(req.body.devoured);
 
     console.log("id of burger to be devoured: ", condition);
 
-    burger.update({ devoured: req.body.devoured }, condition, function (result) {
+    burger.update({ devoured }, condition, function (result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
